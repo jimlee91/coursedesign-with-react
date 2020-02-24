9458da53673e990bfe8fd31dcb49543e93e78d9b
skyrocket <skyrocket.kr@gmail.com>
2020-02-24 19:38:24 +0900

    default

---
+++ pages/_app.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withRedux from 'next-redux-wrapper';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import reducer from '../reducers';
+import rootSaga from '../sagas';
+
+const NodeBird = ({ Component, store }) => {
+  return (
+    <Provider store={store}>
+      <Component />
+    </Provider>
+  );
+};
+
+NodeBird.propTypes = {
+  Component: PropTypes.elementType,
+  store: PropTypes.object,
+};
+
+export default withRedux((initialState, options) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer =
+    process.env.NODE_ENV === 'production'
+      ? compose(applyMiddleware(...middlewares))
+      : compose(
+          applyMiddleware(...middlewares),
+          (!options.isServer &&
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+            compose,
+        );
+  const store = createStore(reducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
+  // 여기에 Store 커스터마이징,
+  return store;
+})(NodeBird);
